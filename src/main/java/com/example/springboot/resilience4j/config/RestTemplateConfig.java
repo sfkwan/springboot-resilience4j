b5a38a7299e1deb93b7662c566ab9718ef1f7aae
Preston Kwan <sfkwan@yahoo.com>
2026-09-12 15:34:11 +0800

    Capture the call out API request and response using logbook

--- src/main/java/com/example/springboot/resilience4j/config/RestTemplateConfig.java
+++ src/main/java/com/example/springboot/resilience4j/config/RestTemplateConfig.java
@@ -6,16 +6,19 @@ import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    public RestTemplate restTemplate(
+            RestTemplateBuilder builder,
+            LogbookClientHttpRequestInterceptor logbookInterceptor) {
         return builder
                 .connectTimeout(Duration.ofSeconds(2))
                 .readTimeout(Duration.ofSeconds(2))
+                .additionalInterceptors(logbookInterceptor)
                 .build();
     }
 }
